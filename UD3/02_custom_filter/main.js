873eca40e45712f8b0619980d8c075c3bc5a0f2e
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-17 18:33:10 +0200

    17ej

--- UD3/02_custom_filter/main.js
+++ UD3/02_custom_filter/main.js
@@ -1,15 +1,26 @@
 const assert = require('assert').strict;
 
-function select(array, condition) {
+function select(array, conditionCallback){
+    let result = []
+
+    for (let index = 0; index < array.length; index++){
+        const element = array[index];
+        if (conditionCallback(element)){
+            result.push(element)
+        }
+        return result
+    }
 }
 
 let values = [1, 2, 3, 5, 7, 13, 17, 23, 29]
 
+let pairs = (values) => values % 2 === 0;
+
 // sólo pares
 assert.deepStrictEqual(select(values, pairs), [2])
 
-// mayores que 15
-assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
+// // mayores que 15
+// assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
 
-// menores de 10
-assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
+// // menores de 10
+// assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
