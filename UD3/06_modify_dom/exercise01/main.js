f29b480678bfa4d561b9fb08843bb784510ec6f9
Roberto Bellido <a25664@svalero.com>
2022-10-26 00:16:17 +0200

    26/10

--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,5 +1,21 @@
 window.onload = function(e) {
     console.log('documento cargado')
+    document.getElementsByTagName('h2')[1].innerText = 'Changed from JS!!'
+    let username = document.getElementById("username");
+    console.log(username);
 
-    // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
+    let a = document.getElementsByClassName("first");
+    a[0].style.color="blue";
+    a[1].style.color="blue";
+
+    let lis = document.getElementsByTagName('li')
+    let items = lis.getElementsByClassName("item");
+    console.log(items);
+
+    let buttons = document.getElementsByTagName('button').getElementsByClassName("buttons");
+    console.log(buttons);
+
+    document.getElementById("color").style.backgroundColor = "#0000FF";
+
+    
 }
