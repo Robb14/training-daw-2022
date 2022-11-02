0fc6c86ac7c4ee2f6c1162d7d37fd389739dfed9
Roberto Bellido <a25664@svalero.com>
2022-11-02 16:22:17 +0100

    01/11

--- UD3/08_forms/main.js
+++ UD3/08_forms/main.js
@@ -1,13 +1,39 @@
-window.onload = function() {
-    console.log(document.main_form.elements)
+window.onload = function () {
+  checkbox1 = document.getElementById("condiciones");
+  checkbox2 = document.getElementById("privacidad");
+  submit = document.getElementById("submit");
+  combo = document.getElementById("opciones");
+  radio = document.getElementById("pregunta_si");
+  submit.style.display = "none"
+  combo.style.display = "none"
+};
 
-    console.log(document.getElementById('pregunta_si').value)
-    console.log(document.getElementById('pregunta_si').checked)
-
-    document.getElementById('pregunta_si').onchange = changeValue
-    document.getElementById('pregunta_no').onchange = changeValue
+function display(){
+    if (checkbox2.checked && checkbox1.checked) {
+        submit.style.display = "block";
+    } else {
+        submit.style.display = "none";
+    }
 }
 
-function changeValue(e) {
-     console.log('changed' + e.target.value)
+function display2(){
+    if (radio.checked) {
+        combo.style.display = "block";
+    } else {
+        combo.style.display = "none";
+    }
 }
+
+function advise(){
+    submit.style.display = "none"
+    if (!document.getElementById("names").value && !document.getElementById("surnames").value){
+        alert("El nombre de usuario y los apellidos son obligatorios");
+    }
+    else if (!document.getElementById("names").value){
+        alert("El nombre de usuario es obligatorio");
+    }
+    else if (!document.getElementById("surnames").value) {
+        alert("El apellido de usuario es obligatorio");
+    }
+}
+function changeValue(e) {}
