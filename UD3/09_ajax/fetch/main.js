3013a9310b21742af99b2b7689ea023a5f8538b3
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-11-07 18:32:53 +0100

    07/11

--- UD3/09_ajax/fetch/main.js
+++ UD3/09_ajax/fetch/main.js
@@ -1,9 +1,40 @@
-window.onload = function() {
+window.onload = function () {
     console.log('document loaded');
 
     fetch('http://dummy.restapiexample.com/api/v1/employees')
         .then(response => response.json())
         .then(response => {
             console.log(response)
+            let employees = response.data
+            for (let i = 0; i < employees.length; i++) {
+                addRow(employees[i])
+            }
         })
+}
+
+function addRow(employee) {
+    let tbody = document.getElementsByTagName('tbody')[0]
+
+    let tr = document.createElement('tr')
+    let td = document.createElement('td')
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = employee.id
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = employee.employee_name
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = employee.employee_salary
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = employee.employee_age
+    tr.appendChild(td)
+
+    tbody.appendChild(tr)
+    console.log('add')
 }
--- UD3/09_ajax/main.js
+++ UD3/09_ajax/main.js
@@ -5,10 +5,40 @@ window.onload = function() {
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            let employees = JSON.parse(this.responseText).data
             console.log(this.responseText)
+            for (let i = 0; i<employees.length; i++){
+                addRow(employees[i])
+            }
         }
       };
 
     xhttp.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true);
     xhttp.send();
+}
+function addRow(employee) {
+    let tbody = document.getElementsByTagName('tbody')[0]
+
+    let tr = document.createElement('tr')
+    let td = document.createElement('td')
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = employee.id
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = employee.employee_name
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = employee.employee_salary
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerText = employee.employee_age
+    tr.appendChild(td)
+
+    tbody.appendChild(tr)
+    console.log('add')
 }
