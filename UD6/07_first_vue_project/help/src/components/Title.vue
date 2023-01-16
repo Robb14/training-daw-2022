c761d3cc06805a48d09586f4fe358b2d1897eb29
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-01-16 18:30:18 +0100

    16/01

---
+++ UD6/07_first_vue_project/help/src/components/Title.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h1>
+        Esto es mi primer componente
+      </h1>
+      Mensaje {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0,
+            msg: 'Hello world!!'
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1{
+    color: red;
+}
+</style>
---
+++ UD6/08_first_vue_component/component/src/components/Title.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <div class="name">
+      Mi nombre completo es: {{ name }} {{ surnames }}
+    </div>
+    <div class="age">
+        Mi edad es: {{ age }}
+    </div>
+    <button @click="age++">Incrementar</button>
+    <button @click="age--">Decrementar</button>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'Roberto',
+            surnames: 'Bellido Marin',
+            age: 20
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .name{
+        background-color: coral;
+    }
+    .age{
+        background-color: lightcyan;
+    }
+</style>
